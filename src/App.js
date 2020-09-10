26da8d3787fd12054622467c7caf552b27241ca1
HajaSC <hajasc@protonmail.com>
2020-09-10 15:01:12 -0400

    Adds logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import cash_symbol from './assets/cash_symbol.png';
 import './styles/main.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={cash_symbol} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
